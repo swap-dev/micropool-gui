71698f4292c574d1cf3c98a5b8942542dc9ecd16
sebseb7 <sebgreenbus@gmail.com>
2019-03-06 06:25:31 +0100

    v11 compatibility

--- main.js
+++ main.js
@@ -102,12 +102,14 @@ function getBlockTemplate(callback){
 }
 
 var current_target    = 0;
+var current_version   = 10;
 var current_height    = 1;
 var current_blob      = "";
 var current_hashblob  = "";
 var previous_hashblob = "";
 var current_prevhash  = "";
 var connectedMiners   = {};
+var jobcounter=0;
 
 function nonceCheck(miner,nonce) {
 
@@ -137,7 +139,7 @@ function updateJob(reason,callback){
 		}
 
 		var previous_hash_buf = Buffer.alloc(32);
-		Buffer.from(result.blocktemplate_blob, 'hex').copy(previous_hash_buf,0,7,39);;
+		Buffer.from(result.blocktemplate_blob, 'hex').copy(previous_hash_buf,0,7,39);
 		var previous_hash = previous_hash_buf.toString('hex');
 		
 
@@ -149,8 +151,10 @@ function updateJob(reason,callback){
 			previous_hashblob = current_hashblob;
 			current_hashblob = result.blockhashing_blob;
 			current_height=result.height;
+			current_version= parseInt(current_hashblob.substring(0,2),16);
+			jobcounter=0;
 
-			logger.info('New block to mine at height '+result.height+' w/ difficulty of '+result.difficulty+' (triggered by: '+reason+')');
+			logger.info('New block v'+current_version+' to mine at height '+result.height+' w/ difficulty of '+result.difficulty+' (triggered by: '+reason+')');
 
 			mainWindow.webContents.send('get-reply', ['data_diff',result.difficulty]);
 			mainWindow.webContents.send('get-reply', ['data_height',result.height]);
@@ -158,8 +162,7 @@ function updateJob(reason,callback){
 			for (var minerId in connectedMiners){
 				var miner = connectedMiners[minerId];
 				miner.nonces = [];
-				var response2 = '{"id":"Stratum","jsonrpc":"2.0","method":"getjobtemplate","result":{"difficulty":'+miner.difficulty+',"height":'+current_height+',"job_id":'+seq()+',"pre_pow":"'+ result.blockhashing_blob +'"},"error":null}';
-				//setTimeout(function(){ miner.socket.write(response2+"\n")},5000);
+				var response2 = '{"id":"Stratum","jsonrpc":"2.0","method":"getjobtemplate","result":{"difficulty":'+miner.difficulty+',"height":'+current_height+',"job_id":'+seq()+',"pre_pow":"'+current_hashblob+miner.nextnonce()+'"},"error":null}';
 				miner.socket.write(response2+"\n");
 			}
 		}
@@ -171,6 +174,8 @@ function Miner(id,socket){
 	this.socket = socket;
 	this.login = '';
 	this.pass = '';
+	this.jobnonce = '';
+	this.oldnonce = '';
 	this.begin = Date.now()/1000|0;
 	this.shares = 0;
 	this.difficulty = 1;
@@ -212,6 +217,20 @@ Miner.prototype.respose = function (result,error,request) {
 	});
 	logger.debug("p->m "+response);
 	this.socket.write(response+"\n");
+}
+
+Miner.prototype.nextnonce = function () {
+
+	this.oldnonce = this.jobnonce;
+	if( current_version >= 11) {
+		var noncebuffer = Buffer.allocUnsafe(4);
+		noncebuffer.writeUInt32BE(++jobcounter,0);
+		this.jobnonce = '00000000'+noncebuffer.toString('hex');
+	}
+	else{
+		this.jobnonce = '';
+	}
+	return this.jobnonce;
 }
 	
 function handleClient(data,miner){
@@ -258,32 +277,42 @@ function handleClient(data,miner){
 		}
 		var noncebuffer = Buffer.allocUnsafe(4);
 		noncebuffer.writeUInt32BE(request.params.nonce,0);
-		var header = Buffer.concat([Buffer.from(current_hashblob, 'hex'),noncebuffer]);
+		var header = Buffer.concat([Buffer.from(current_hashblob, 'hex'),Buffer.from(miner.jobnonce,'hex'),noncebuffer]);
 			
 		if(verify_c29s(header,header.length,cycle)){
 
-			var header_previous = Buffer.concat([Buffer.from(previous_hashblob, 'hex'),noncebuffer]);
+			var header_previous = Buffer.concat([Buffer.from(previous_hashblob, 'hex'),Buffer.from(miner.oldnonce,'hex'),noncebuffer]);
 			
 			if(verify_c29s(header_previous,header_previous.length,cycle)){
-
-				logger.info('stale ('+miner.login+')');
-				return miner.respose('stale',null,request);
+			
+				logger.info('wrong hash or very old ('+miner.login+') '+request.params.height);
+				return miner.respose(null,{code: -32502, message: "wrong hash"},request);
 			}
 			else{
 
-				logger.info('wrong hash or very old ('+miner.login+') '+request.params.height);
-				return miner.respose(null,{code: -32502, message: "wrong hash"},request);
+				logger.info('stale ('+miner.login+')');
+				return miner.respose('stale',null,request);
 			}
 		}
 		
 		if(check_diff(current_target,cycle)) {
 			
 			var block = Buffer.from(current_blob, 'hex');
-			for(var i in request.params.pow)
-			{
-				block.writeUInt32LE(request.params.pow[i], 43+(i*4));
+			if( current_version >= 11) {
+				for(var i in request.params.pow)
+				{
+					block.writeUInt32LE(request.params.pow[i], 51+(i*4));
+				}
+				block.writeUInt32LE(request.params.nonce,47);
+				Buffer.from(miner.jobnonce, 'hex').reverse().copy(block,39,0,8);
 			}
-			block.writeUInt32LE(request.params.nonce,39);
+			else{
+				for(var i in request.params.pow)
+				{
+					block.writeUInt32LE(request.params.pow[i], 43+(i*4));
+				}
+				block.writeUInt32LE(request.params.nonce,39);
+			}
 
 			rpc('submitblock', [block.toString('hex')], function(error, result){
 				logger.info('BLOCK ('+miner.login+')');
@@ -311,7 +340,7 @@ function handleClient(data,miner){
 	
 	if(request && request.method && request.method == "getjobtemplate") {
 		
-		return miner.respose({difficulty:parseFloat(miner.difficulty),height:current_height,job_id:parseFloat(seq()),pre_pow:current_hashblob},null,request);
+		return miner.respose({difficulty:parseFloat(miner.difficulty),height:current_height,job_id:parseFloat(seq()),pre_pow:current_hashblob+miner.nextnonce()},null,request);
 	}
 	else{
 
