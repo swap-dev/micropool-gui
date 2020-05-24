0793ba96cfc9dc780c4843cb9045533eaad33008
LPHuynh <30508709+LPHuynh@users.noreply.github.com>
2020-05-24 16:15:33 -0400

    more stats

--- main.js
+++ main.js
@@ -103,6 +103,7 @@ function getBlockTemplate(callback){
 
 var current_target    = 0;
 var current_height    = 1;
+var current_reward    = 0;
 var current_blob      = "";
 var current_hashblob  = "";
 var previous_hashblob = "";
@@ -166,6 +167,7 @@ function updateJob(reason,callback){
 			current_blob     = result.blocktemplate_blob;
 			current_hashblob = result.blockhashing_blob.slice(0,-16);	
 			current_height   = result.height;
+            current_reward   = result.expected_reward / Math.pow (10,12);
 			
 			jobcounter++;
 
@@ -173,6 +175,9 @@ function updateJob(reason,callback){
 
 			mainWindow.webContents.send('get-reply', ['data_diff',result.difficulty]);
 			mainWindow.webContents.send('get-reply', ['data_height',result.height]);
+            mainWindow.webContents.send('get-reply', ['data_netgraphrate', (current_target / 1500 * 32).toFixed(2) + 'KGps' ]);
+            mainWindow.webContents.send('get-reply', ['data_reward',current_reward.toFixed(2) + ' XWP']);
+
 		
 			for (var minerId in connectedMiners){
 				var miner = connectedMiners[minerId];
@@ -352,8 +357,26 @@ function handleClient(data,miner){
 		
 			shares+=parseFloat(miner.difficulty);
 			jobshares+=parseFloat(miner.difficulty);
-			mainWindow.webContents.send('get-reply', ['data_shares',shares+' ('+(jobshares/current_target*100).toFixed(2)+'%)']);
-				
+			mainWindow.webContents.send('get-reply', ['data_shares',shares]);
+            mainWindow.webContents.send('get-reply', ['data_currenteffort',(jobshares/current_target*100).toFixed(2)+'%']);
+            
+            var totalgps=0;
+            for (var minerId in connectedMiners){
+                var miner2 = connectedMiners[minerId];
+                totalgps+=miner2.gps;
+            }
+            var etaTime = new Date(0);
+            if (totalgps)
+            {
+                etaTime.setSeconds(parseInt(current_target/totalgps * 32));
+            }
+            else
+            {
+                etaTime.setSeconds(0)
+            }
+            mainWindow.webContents.send('get-reply', ['data_blocketa', etaTime.toISOString().substr(11, 8)+'s']);
+            mainWindow.webContents.send('get-reply', ['data_revenue', ((totalgps * 86400 / current_target) * (current_reward / 32)).toFixed(2) +' xwp']);
+
 			logger.info('share ('+miner.login+') '+miner.difficulty+' ('+hashrate(miner)+')');
 			return miner.respose('ok',null,request);
 		}
@@ -397,8 +420,8 @@ function createWindow () {
 	// Create the browser window.
 	mainWindow = new BrowserWindow({
 		title: 'Swap Micropool',
-		width: 800,
-		height: 600,
+		width: 1000,
+		height: 800,
 		minWidth: 800,
 		minHeight: 310,
 		icon: __dirname + '/build/icon_small.png'
