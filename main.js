ba79f760e9921154d8a01b5550824e18f21ba349
LPHuynh <30508709+LPHuynh@users.noreply.github.com>
2020-05-24 17:29:07 -0400

    hashrate fix

--- main.js
+++ main.js
@@ -175,7 +175,7 @@ function updateJob(reason,callback){
 
 			mainWindow.webContents.send('get-reply', ['data_diff',result.difficulty]);
 			mainWindow.webContents.send('get-reply', ['data_height',result.height]);
-			mainWindow.webContents.send('get-reply', ['data_netgraphrate', (current_target / 1500 * 32).toFixed(2) + 'KGps' ]);
+			mainWindow.webContents.send('get-reply', ['data_netgraphrate', (current_target / 15000 * 32).toFixed(2) + 'KGps' ]);
 			mainWindow.webContents.send('get-reply', ['data_reward',current_reward.toFixed(2) + ' XWP']);
 
 		
@@ -375,7 +375,7 @@ function handleClient(data,miner){
 				etaTime.setSeconds(0)
 			}
 			mainWindow.webContents.send('get-reply', ['data_blocketa', etaTime.toISOString().substr(11, 8)+'s']);
-			mainWindow.webContents.send('get-reply', ['data_revenue', ((totalgps * 86400 / current_target) * (current_reward / 32)).toFixed(2) +' xwp']);
+			mainWindow.webContents.send('get-reply', ['data_revenue', ((totalgps * 86400 / current_target) * (current_reward / 32)).toFixed(2) +' XWP']);
 
 			logger.info('share ('+miner.login+') '+miner.difficulty+' ('+hashrate(miner)+')');
 			return miner.respose('ok',null,request);
